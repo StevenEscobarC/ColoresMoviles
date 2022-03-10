235c4dff73f87c777f8756efa93a0d088a8369e3
StevenEscobarC <stevenlikeplis@gmail.com>
2022-03-10 15:22:45 -0500

    Cambios en la controladora MainActivity

--- app/src/main/java/com/stevenescobar/colors/MainActivity.java
+++ app/src/main/java/com/stevenescobar/colors/MainActivity.java
@@ -2,6 +2,7 @@ package com.stevenescobar.colors;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.SeekBar;
@@ -32,19 +33,61 @@ public class MainActivity extends AppCompatActivity {
     public void initViews(){
        lblTargetColor = findViewById(R.id.lblTargetColor);
        lblProposedColor = findViewById(R.id.lblProposedColor);
+
        sbrRed = findViewById(R.id.sbrRed);
        sbrGreen = findViewById(R.id.sbrGreen);
        sbrBlue = findViewById(R.id.sbrBlue);
+
        lblRedValue = findViewById(R.id.lblRedValue);
        lblGreenValue = findViewById(R.id.lblGreenValue);
        lblBlueValue = findViewById(R.id.lblBlueValue);
+
        btnGetScore = findViewById(R.id.btnGetScore);
        btnNewColor = findViewById(R.id.btnNewColor);
 
     }
 
     public void  initEvents(){
+        SeekBar[] seekBars = {sbrRed, sbrGreen, sbrBlue};
+
+        for(SeekBar seekBar: seekBars){
+            seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+
+                }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+
+                }
+            });
+        }
+
+        //btnGetScore.setOnClickListener(view -> showScore());
+        //btnGetScore.setOnClickListener(view -> restartScore());
+    }
+
+    public void restartGame(){
+        colorsGame.restartGame();
+    }
+
+    public void updateValues(){
+
+        int redValue = sbrRed.getProgress();
+        int greenValue = sbrRed.getProgress();
+        int blueValue = sbrRed.getProgress();
+        int newBackColor = Color.rgb(redValue, greenValue, blueValue);
+
+        colorsGame.setProposedBackColor(newBackColor);
 
     }
+
+
 
 }
